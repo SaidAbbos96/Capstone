f0d0dc0f58850a38222533143e032a720583a066
SaidAbbos Khudoykulov <54773414+SaidAbbos96@users.noreply.github.com>
2020-05-07 14:18:34 +0500

    My Capstone Project !

---
+++ app.py
@@ -0,0 +1,182 @@
+import os
+from flask import Flask, request, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from models import setup_db, Reservations, Restaurant, MenuItems
+from auth import requires_auth
+
+
+def create_app(test_config=None):
+  # create and configure the app
+  app = Flask(__name__)
+  setup_db(app)
+  CORS(app)
+
+  @app.route('/')
+  def get_greeting():
+    return 'hi, i`m SaidAbbos. abbos.xudoyqulov@mail.ru !<br><a href="infomir.uz">Login</a>'
+
+  # Public - get:restaurant
+  @app.route('/restaurants')
+  def get_restaruants():
+    try:
+      restaurants = Restaurant.query.all()
+      if menu_items == []:
+        abort(404)
+    except:
+      abort(404)
+    return jsonify([restaurant.format() for restaurant in restaurants])
+
+  # Public - get:menu_item
+  @app.route('/restaurants/<int:id>/menu')
+  def get_restaruant_menu(id):
+    r_id = id
+    try:
+      menu_items = MenuItems.query.filter(MenuItems.restaurant_id==r_id).all()
+      if menu_items == []:
+        abort(404)
+    except:
+      abort(404)
+    return jsonify([item.format() for item in menu_items])
+
+  
+  # Customer - post:reservation
+  @app.route('/restaurants/<int:id>/reservation', methods=['POST'])
+  @requires_auth('post:reservation')
+  def post_reservation(token, id):
+    try:
+      restaurant_id = id
+
+      body = request.get_josn()
+      time_of_res = body.get('time_of_res')
+      num_of_people = body.get('num_of_people')
+      name_for_res = body.get('name_for_res')
+
+      customer_id = token.get('sub')
+
+      reservation = Reservations(restaurant_id=restaurant_id, time_of_res=time_of_res, num_of_people=num_of_people, name_for_res=name_for_res, customer_id=customer_id)
+      reservation.insert()
+
+      upcoming_reservations = Reservations.query.filter(Reservations.customer_id==customer_id,time_of_res >= datetime.now())
+
+    except:
+      abort(403)
+
+    return [reservation.format() for reservation in upcoming_reservations]
+
+  
+  # Restaurant owner - post:restaurant
+  @app.route('/restaurants', methods=['POST'])
+  @requires_auth('post:restaurant')
+  def post_restaurants(token):
+    try:
+
+      owner_id = token.get('sub')
+
+      body = request.get_json()
+      name = body.get('name')
+      address = body.get('address')
+
+      restaurant = Restaurant(owner_id=owner_id, name=name, address=address)
+      restaurant.insert()
+
+      users_restaurant = Restaurant.query.filter(Restaurant.owner_id==owner_id).all()
+
+    except:
+      abort(422)
+
+    return jsonify([restaurant.format() for restaurant in users_restaurant])
+
+
+    # Restaurant owner - patch:restaurant
+    @app.route('/restaurants/<int:id>', methods=['POST'])
+    @requires_auth('patch:restaurant')
+    def edit_restaurant(token, id):
+      try:
+        owner_id = token.get('sub')
+
+        restaurant = Restaurant.query.get(id)
+        
+        if owner_id != restaurant.owner_id:
+          abort(401)
+
+        body = request.get_json()
+
+        new_name = body.get('name', None)
+        new_address = body.get('address', None)
+    
+        if new_name:
+          restaurant.name = new_name
+        if new_address:
+          restaurant.address = new_address
+
+        restaurant.update()
+
+        updated_restaurant = Restaurant.query.get(id)
+      
+      except:
+        abort(422)
+
+      return jsonify(updated_restaurant.format())
+
+
+  # Restaurant owner - delete:restaurant
+  @app.route('/restaurants/<int:id>', methods=['POST'])
+  @requires_auth('delete:restaurant')
+  def delete_restaurant(token, id):
+    try:
+      owner_id = token.get('sub')
+
+      restaurant = Restaurant.query.get(id)
+      
+      if owner_id != restaurant.owner_id:
+        abort(401)
+
+      restaurant.delete()
+    except:
+      abort(422)
+
+  # Error Handlers
+  
+  @app.errorhandler(422)
+  def unprocessable(error):
+      return jsonify({
+                      "success": False,
+                      "error": 422,
+                      "message": "unprocessable"
+                      }), 422
+
+
+  @app.errorhandler(404)
+  def not_found(error):
+      return jsonify({
+          "success": False,
+          "error": 404,
+          "message": "resource not found"
+      }), 404
+
+
+  @app.errorhandler(401)
+  def unauthorized(error):
+      return jsonify({
+          "success": False,
+          "error": 401,
+          "message": "Unauthorized Error"
+      }), 401
+
+  @app.errorhandler(400)
+  def bad_request(error):
+      return jsonify({
+          "success": False,
+          "error": 400,
+          "message": "Bad Request"
+      }), 400
+
+
+
+  return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
---
+++ models.py
@@ -0,0 +1,106 @@
+# imports
+from sqlalchemy import Column, String, create_engine, Integer, DateTime, Numeric, ForeignKey
+from flask_sqlalchemy import SQLAlchemy
+import json
+import os
+
+database_path = os.environ['DATABASE_URL']
+#local db url set DATABASE_URL=postgresql://postgres:1111@localhost:5432/capstone
+db = SQLAlchemy()
+
+# setup db
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+# table restaurants
+class Restaurant(db.Model):
+    __tablename__ = 'restaurants'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    address = Column(String)
+    owner_id = Column(String, nullable=False)  # owner_id will be retrieved from token to see which user owns the restaurant
+    reservation_rel = db.relationship('Reservations', backref='restaurnt_res', lazy=True)
+    reservation_rel = db.relationship('MenuItems', backref='restaurnt_menu_item', lazy=True)  
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+             "name": self.name
+            ,"address": self.address
+        }
+
+
+# Table to store information about the reservations at restaurants
+class Reservations(db.Model):
+    __tablename__ = 'reservations'
+
+    id = Column(Integer, primary_key=True)
+    time_of_res = Column(DateTime)
+    num_of_people = Column(Integer)
+    name_for_res = Column(String) 
+    customer_id = Column(String) # customer_id will be retrieved from token
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+             "time_of_res": self.time_of_res
+            ,"num_of_people": self.num_of_people
+            ,"restaurant_name": self.restaurnt_res.name
+        } 
+
+
+
+# Table to store information about the Menu Items at restaurants
+class MenuItems(db.Model):
+    __tablename__ = 'menu_items'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+    price = Column(Numeric)
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+    def format(self):
+        return {
+             "name": self.name
+            ,"description": self.description
+            ,"price": str(self.price)
+            ,"restaurant_name": self.restaurnt_menu_item.name
+        }
